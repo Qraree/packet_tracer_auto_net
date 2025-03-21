5decd6f6dff0bdb4833c66d6660c2c61974d59ee
qraree <metbezu@gmail.com>
2025-03-21 23:57:31 +0300

    feat: router config

--- src/main/java/org/core/gui/controllers/ConfigurationPageController.java
+++ src/main/java/org/core/gui/controllers/ConfigurationPageController.java
@@ -97,14 +97,21 @@ public class ConfigurationPageController implements Initializable {
             switchPort.setAccessVlan(subnetIndex);
           }
 
-          Device otherDevice = link.getPort1().getOwnerDevice();
-          System.out.println(otherDevice.getName());
+          // dto router fix
+          Port otherPort = link.getPort1();
+          Port netDevicePort = link.getPort2();
+          Device otherDevice = otherPort.getOwnerDevice();
+          Device netDevicce = netDevicePort.getOwnerDevice();
+          String PortName = netDevicce.getName();
+
+          System.out.println(netDevicce.getName());
 
           boolean isPersonalDevice =
               Arrays.asList(DeviceType.PC, DeviceType.LAPTOP).contains(otherDevice.getType());
 
           if (isPersonalDevice) {
-            IPAddress defaultGatewayAddress = new IPAddressImpl("192.168.1.0");
+            IPAddress defaultGatewayAddress =
+                new IPAddressImpl("192.168." + (subnetIndex - 1) + ".1");
             IPAddress subnetMask = new IPAddressImpl("255.255.255.0");
             IPAddress subnetAddress =
                 new IPAddressImpl("192.168." + (subnetIndex - 1) + "." + pcIndex);
@@ -113,6 +120,27 @@ public class ConfigurationPageController implements Initializable {
             hostPort.setIpSubnetMask(subnetAddress, subnetMask);
             hostPort.setDefaultGateway(defaultGatewayAddress);
             pcIndex++;
+          }
+
+          boolean isRouterDevice = otherDevice.getType() == DeviceType.ROUTER;
+
+          if (isRouterDevice
+              && otherDevice instanceof Router router
+              && otherPort instanceof RouterPort routerPort) {
+
+            System.out.println("Router " + router.getName() + " " + routerPort.getName());
+
+            if (port instanceof SwitchPort switchPort) {
+              switchPort.setAccessPort(false);
+            }
+
+            routerPort.setPower(true);
+            router.addSubInt(PortName, subnetIndex);
+            router.changePortEncapsulation(PortName, "dot1Q");
+
+            IPAddress subnetMask = new IPAddressImpl("255.255.255.0");
+            IPAddress subnetAddress = new IPAddressImpl("192.168." + (subnetIndex - 1) + ".1");
+            routerPort.setIpSubnetMask(subnetAddress, subnetMask);
           }
         }
       }
